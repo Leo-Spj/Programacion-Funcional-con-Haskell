d811be55582bcff326a07d26cfef56c99cea926e
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-04-05 11:12:04 -0500

    Agregar funciones de programación funcional en Haskell

---
+++ funciones/Test.hs
@@ -0,0 +1,25 @@
+module Test where
+
+-- Haskell - Programacion funcional
+
+suma :: Int -> Int -> Int
+suma a b = a + b
+
+-- Funciones prefijas, infijas y postfijas
+
+-- Funciones prefijas
+-- Son funciones que se escriben con el nombre de la funcion seguido de los argumentos
+-- Ejemplo: suma 1 2
+
+-- Funciones infijas
+-- Son funciones que se escriben con el nombre de la funcion entre los argumentos
+-- Ejemplo: 1 `suma` 2
+
+-- Funciones postfijas
+-- Son funciones que se escriben con los argumentos seguidos del nombre de la funcion
+-- Ejemplo: 1 2 suma
+
+
+-- Funciones de orden superior
+-- Son funciones que reciben como argumento otra funcion
+-- Ejemplo: map, filter, foldl, foldr, etc
