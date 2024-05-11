0863e7439f13385526bf0fdab9f5618ff80b47d5
Leonardo Espejo <85245534+leo-spj@users.noreply.github.com>
2024-05-10 21:43:39 -0500

    Agregar módulo Tuplas.hs con funciones relacionadas a tuplas

---
+++ semanas/S7/tuplas.hs
@@ -0,0 +1,36 @@
+module Tuplas where
+
+
+-- Tipo de dato que pued tener elementos de diferentes tipo, 
+-- lista de tuplas el tamaña y tipo de la tupla deben ser iguales
+
+-- Duplas
+
+-- fst (1,2) -> 1
+-- primero (a, b) = a
+
+-- snd (1,2) -> 2
+-- segundo (a, b) = b
+
+-- Lista de tuplas:
+-- [(23, "Juan"), (17,"María")] : esto es correcto
+-- [(23, "Juan"), (17, 33)] :  esto es incorrecto
+
+-- Tupla de tuplas:
+-- ((23, "Juan"), ("hola","María"), True, 10) : esto es correcto
+
+
+-- triangulos rectangulos de triangulos rectangulos menores lado 100
+-- creando una lista de tuplas
+triangulos = [(a,b,c) | c <- [1..100], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+
+listaTrue [] = True
+listaTrue (x:xs) = fst x && listaTrue xs
+
+-- no hay recursividad en tuplas
+
+allTrueTupla (x, y) = x && y
+
+
+-- [("Jorge", 13), ("Maria", 16), ("Juan", 11)]
+aprobados lista notaMin = [ nombre | (nombre, nota) <- lista, nota >= notaMin]
