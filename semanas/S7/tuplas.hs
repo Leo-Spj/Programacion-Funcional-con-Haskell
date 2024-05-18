cfb29ded3a5ce009ef7ed30231c65fb35d2c9d6f
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-05-18 18:44:23 -0500

    fix: comparacion ==

--- semanas/S7/tuplas.hs
+++ semanas/S7/tuplas.hs
@@ -47,6 +47,6 @@ matrizCuadrada lista1 lista2 = [(x,y) | x <- lista1, y <- lista2]
 -- [(1,1), (2,2), (3,3)]
 matrizTriangularSuperior lista = [(fst x, snd y) | x <- lista, y <- lista, fst x <= fst y] -- Superior
 matrizTriangularSuperior lista = [(fst x, snd y) | x <- lista, y <- lista, fst x >= fst y] -- Inferior
-matrizTriangularSuperior lista = [(fst x, snd y) | x <- lista, y <- lista, fst x = fst y] -- Triangular
+matrizTriangularSuperior lista = [(fst x, snd y) | x <- lista, y <- lista, fst x == fst y] -- Triangular
 
 
